849015b938484b36abe03432985299f37db4abb5
Israel Borges <israelborges@zertico.com>
2015-08-06 13:56:45 -0300

    Correct names on create_resources

--- manifests/iis_hiera.pp
+++ manifests/iis_hiera.pp
@@ -18,31 +18,31 @@ class iis::iis_hiera(
   
   if ( $app_pool_hash != {} ){
     validate_hash($app_pool_hash)
-    create_resources('iis::app_pool',$app_pool_hash)
+    create_resources('iis::manage_app_pool',$app_pool_hash)
   }
   
   if ( $binding_hash != {} ){
     validate_hash($binding_hash)
-    create_resources('iis::binding',$binding_hash)
+    create_resources('iis::manage_binding',$binding_hash)
   }
   
   if ( $site_state_hash != {} ){
     validate_hash($site_state_hash)
-    create_resources('iis::site_state',$site_state_hash)
+    create_resources('iis::manage_site_state',$site_state_hash)
   }
   
   if ( $site_hash != {} ){
     validate_hash($site_hash)
-    create_resources('iis::site',$site_hash)
+    create_resources('iis::manage_site',$site_hash)
   }
   
   if ( $vapplication_hash != {} ){
     validate_hash($vapplication_hash)
-    create_resources('iis::virtual_application',$vapplication_hash)
+    create_resources('iis::manage_virtual_application',$vapplication_hash)
   }
   
   if ( $vdirectory_hash != {} ){
     validate_hash($vdirectory_hash)
-    create_resources('iis::virtual_directory',$vdirectory_hash)
+    create_resources('iis::manage_virtual_directory',$vdirectory_hash)
   }
 }
